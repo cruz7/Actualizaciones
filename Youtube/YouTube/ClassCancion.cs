e912aacab754c722886df90af444fad267bca0f2
Cruz Aguilar <cruzSurvolt_trct@outlook.es>
2019-06-03 12:45:03 -0600

    Prueba 1

---
+++ Youtube/YouTube/ClassCancion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using YoutubeExplode;
+using YoutubeExplode.Models.MediaStreams;
+namespace YouTube
+{
+    class ClassCancion
+    {
+        string nombreCancion;
+        DateTime fechaDescarga;
+
+        public string NombreCancion
+        {
+            get
+            {
+                return nombreCancion;
+            }
+
+            set
+            {
+                nombreCancion = value;
+            }
+        }
+
+        public DateTime FechaDescarga
+        {
+            get
+            {
+                return fechaDescarga;
+            }
+
+            set
+            {
+                fechaDescarga = value;
+            }
+        }
+    }
+}
